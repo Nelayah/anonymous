24276e9b02c9e55b8ddba6579082591dd6e83d84
pengjiasheng <pengjiasheng@cvte.com>
2020-11-06 09:17:32 +0800

    refactor: 接口鉴权

--- _server/middleware/authentication/index.js
+++ _server/middleware/authentication/index.js
@@ -9,29 +9,35 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const jsonwebtoken = require("jsonwebtoken");
+const _cache_1 = require("@cache");
+const system_1 = require("@config/system");
+const errRepText = { code: -1, status: 401, msg: '登录状态已过期，请重新登录' };
+const errCallback = ctx => {
+    ctx.status = 401;
+    ctx.body = errRepText;
+};
 /**
- * 登录状态和JWT有效校验，如果带有 tokenId 则自动生成特殊 JWT
+ * 登录状态与 JWT 有效校验
  *
  * @param {Object} ctx koa context
  * @param {function} next Promise 函数
  */
 exports.default = (ctx, next) => __awaiter(void 0, void 0, void 0, function* () {
-    const isIfNeedLogin = !ctx.config.noAuthApis.reduce((prev, current) => prev || new RegExp(current).test(ctx.originalUrl), false);
-    const { sso } = ctx.config;
-    const timeOut = {
-        errCode: 401,
-        errMsg: '登录超时，请重新登录',
-        redirectUrl: `${sso.baseUrl}/logout?service=${sso.service}`
-    };
-    const exists = yield ctx.services.auth.authTokenExists();
-    if (isIfNeedLogin && !exists)
-        return ctx.messages(timeOut);
+    const cacheKey = ctx.cookies.get(system_1.COOKIE_KEY);
+    // cookie 不存在
+    if (cacheKey)
+        return errCallback(ctx);
+    const jwt = _cache_1.default.get(cacheKey);
+    // jwt 不存在
+    if (!jwt)
+        return errCallback(ctx);
+    // jwt 校验不通过
+    try {
+        jsonwebtoken.verify(jwt, system_1.JWT_PRIVATE_KEY);
+    }
+    catch (e) {
+        return errCallback(ctx);
+    }
     yield next();
-    // JWT过期
-    if (ctx.body.errCode !== '4100004')
-        return;
-    const authToken = ctx.cookies.get(ctx.config.authToken);
-    yield ctx.redis.del(authToken);
-    ctx.cookies.set(ctx.config.authToken, '');
-    return ctx.messages(timeOut);
 });
--- _server/middleware/authentication/index.ts
+++ _server/middleware/authentication/index.ts
@@ -1,25 +1,38 @@
-import * as R from 'ramda';
+import * as jsonwebtoken from 'jsonwebtoken';
+import cache from '@cache';
+import {
+  COOKIE_KEY,
+  JWT_PRIVATE_KEY
+} from '@config/system';
+
+const errRepText = { code: -1, status: 401, msg: '登录状态已过期，请重新登录' };
+const errCallback = ctx => {
+  ctx.status = 401;
+  ctx.body = errRepText;
+};
 /**
- * 登录状态和JWT有效校验，如果带有 tokenId 则自动生成特殊 JWT
+ * 登录状态与 JWT 有效校验
  *
  * @param {Object} ctx koa context
  * @param {function} next Promise 函数
  */
 export default async (ctx: Types.KoaContext, next) => {
-  const isIfNeedLogin = !ctx.config.noAuthApis.reduce((prev, current) => prev || new RegExp(current).test(ctx.originalUrl), false);
-    const {sso} = ctx.config;
-    const timeOut = {
-      errCode: 401,
-      errMsg: '登录超时，请重新登录',
-      redirectUrl: `${sso.baseUrl}/logout?service=${sso.service}`
-    };
-    const exists = await ctx.services.auth.authTokenExists();
-    if (isIfNeedLogin && !exists) return ctx.messages(timeOut);
-    await next();
-    // JWT过期
-    if (ctx.body.errCode !== '4100004') return;
-    const authToken = ctx.cookies.get(ctx.config.authToken);
-    await ctx.redis.del(authToken);
-    ctx.cookies.set(ctx.config.authToken, '');
-    return ctx.messages(timeOut);
+  const cacheKey = ctx.cookies.get(COOKIE_KEY);
+
+  // cookie 不存在
+  if (cacheKey) return errCallback(ctx);
+
+  const jwt = cache.get(cacheKey);
+
+  // jwt 不存在
+  if (!jwt)  return errCallback(ctx);
+
+  // jwt 校验不通过
+  try {
+    jsonwebtoken.verify(jwt, JWT_PRIVATE_KEY);
+  } catch (e) {
+    return errCallback(ctx);
+  }
+
+  await next();
 };
